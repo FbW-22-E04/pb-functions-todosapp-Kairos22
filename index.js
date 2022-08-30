69e9a55f11ffca100c92a6ab9f2bb3d2af9c5934
Kairos22 <alandiko84@gmail.com>
2022-08-30 16:13:42 +0200

    working on

--- index.js
+++ index.js
@@ -1,6 +1,61 @@
 const toDosStore = ["Shopping", "Home work", "Go to the gym"];
 
-// Write your code here
+const insertTodo = (newTodo) => toDosStore.push(newTodo);
+
+const createTodos = (todos) => {
+    for (let i = 0; i < todos.length; i++){
+        insertTodo(todos[i]);
+    }
+};
+
+const removeTodo = (orderNumber) => toDosStore.splice(orderNumber - 1, 1);
+
+const editTodo = (orderNumber, newValue) => (toDosStore[orderNumber - 1] = newValue);
 
 
-console.log(RenderToDosListTemplate())
+
+
+
+
+
+
+
+insertTodo("clean Kitchen");
+insertTodo("prepare the Food");
+console.log(toDosStore);
+
+removeTodo(3);
+console.log(toDosStore);
+
+editTodo(3, "Pay bills");
+console.log(toDosStore);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log(RenderToDosListTemplate())
